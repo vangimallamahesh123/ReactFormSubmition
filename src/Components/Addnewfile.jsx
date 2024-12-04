b5d17b76c04d7ad1f4b61f99bcc4bd204cba2799
mahesh <vangimallamahesh828@gmail.com>
2024-12-04 16:39:25 +0530

    dfgh

--- src/Components/Addnewfile.jsx
+++ src/Components/Addnewfile.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Addnewfile() {
   return (
-    <div>Addnewfile</div>
+    <>
+    <h1>this is text file </h1>
+    <h2>this is pratice of  file push git hub</h2>
+    <h1>hello i am mahesh</h1>
+    
+    </>
   )
 }
 
