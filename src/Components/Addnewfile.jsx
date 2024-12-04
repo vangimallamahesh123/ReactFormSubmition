3848d30a506069af78d6b00696db65fcc60f3d48
mahesh <vangimallamahesh828@gmail.com>
2024-12-04 16:41:04 +0530

    third commet

--- src/Components/Addnewfile.jsx
+++ src/Components/Addnewfile.jsx
@@ -3,10 +3,7 @@ import React from 'react'
 function Addnewfile() {
   return (
     <>
-    <h1>this is text file </h1>
-    <h2>this is pratice of  file push git hub</h2>
     <h1>hello i am mahesh</h1>
-    
     </>
   )
 }
